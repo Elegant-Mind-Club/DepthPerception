5504103a2730e301ebda4127759076872bb6d6db
Edmond <mredmondwang@gmail.com>
2023-10-24 11:29:45 -0700

    Add colour extraction and superimposition

--- edgeDetection.py
+++ edgeDetection.py
@@ -6,7 +6,6 @@ from skimage.metrics import structural_similarity as ssim
 
 # Constants
 IMG1_PATH = 'left.JPG'
-
 IMG2_PATH = 'right.JPG'
 
 IMG_CROP_START_X = 1250
@@ -14,12 +13,9 @@ IMG_CROP_START_Y = 1650
 IMG_CROP_WIDTH = 800
 IMG_CROP_HEIGHT = 450
 
-# IMG_CROP_START_X = 1300
-# IMG_CROP_START_Y = 1700
-# IMG_CROP_WIDTH = 300
-# IMG_CROP_HEIGHT = 200
-
-KERNEL_SIZE = 2
+# best practice is 1,3,5,7
+KERNEL_SIZE = 3
+TOLERANCE = 1e-5
 
 
 def mse(patch1, patch2):
@@ -31,29 +27,74 @@ def mse(patch1, patch2):
 
 def initilizeImage(imgPath, imgCropStartX, imgCropStartY, imgCropWidth, imgCropHeight):
     # Return cropped image
-
     return cv2.imread(imgPath)[imgCropStartY:imgCropStartY+imgCropHeight, imgCropStartX:imgCropStartX+imgCropWidth]
 
 
+def extractColors(img1, img2, color):
+
+    #convert the BGR image to HSV colour space
+    hsv1 = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV)
+    hsv2 = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
+
+    # Set the lower and upper bounds for the color hues - need to find this online
+    lower_red = np.array([0, 100, 100])
+    upper_red = np.array([10, 255, 255])
+    lower_green = np.array([50,100,50])
+    upper_green = np.array([70,255,255])
+    lower_blue = np.array([60, 35, 140]) 
+    upper_blue = np.array([180, 255, 255]) 
+
+    match color:
+        case 'red':
+            lower_color = lower_red
+            upper_color = upper_red
+        case 'green':
+            lower_color = lower_green
+            upper_color = upper_green
+        case 'blue':
+            lower_color = lower_blue
+            upper_color = upper_blue
+        case 'white':
+            lower_color = lower_white
+            upper_color = upper_white
+        case 'black':
+            lower_color = lower_black
+            upper_color = upper_white
+        case _:
+            print("Error in color")
+
+
+    # Create a mask for the selected colour using inRange function
+    mask1 = cv2.inRange(hsv1, lower_color, upper_color)
+    mask2 = cv2.inRange(hsv2, lower_color, upper_color)
+
+    # Perform bitwise and on the original image arrays using the mask
+    result1 = cv2.bitwise_and(img1, img1,mask=mask1)
+    result2 = cv2.bitwise_and(img2, img2,mask=mask2)
+
+    return result1, result2
+
+# Color
 def visualCortexV1():
-    # Color
     pass
 
 
-def visualCortexV2V3(img):
-    # Curves and lines
+# Curves and lines
+def detectEdges(img):
     # Convert to graycsale
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Blur the image for better edge detection
-    img_blur = cv2.GaussianBlur(img_gray, (3, 3), 0)
-    # Canny Edge Detection
+    img_blur = cv2.GaussianBlur(img_gray, (5,5), 0)
+    # Canny Edge Detection. 
+    # 5 step process: image smoothening -> finding intensity gradients -> 
+    # non-max suppression -> double threshold -> hysteresis edge tracking
     edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)
     return edges
 
 
-def visualCortexV4(img1, img2):
+def convolute(img1, img2, kernel_size):
     # Depth
-    return find_best_match(img1, img2, KERNEL_SIZE)
+    return find_best_match(img1, img2, kernel_size)
 
 
 def find_best_match(image1, image2, kernel_size):
@@ -69,7 +110,7 @@ def find_best_match(image1, image2, kernel_size):
             best_mse = float('inf')  # Initialize with a high value for MSE
             best_mse_x2 = 0
 
-            # Only slide kernel if there is an edge
+            # Only slide kernel if there is an edge in img1
             if np.mean(patch1) != 0:
                 for x2 in range(x1, width - kernel_size + 1):
                     patch2 = image2[y:y + kernel_size, x2:x2 + kernel_size]
@@ -84,11 +125,12 @@ def find_best_match(image1, image2, kernel_size):
                 match_map[y, x1] = best_mse
 
                 # If MSE is zero (or very close to zero), store the location
-                if np.isclose(best_mse, 0, atol=1e-10):
+                if np.isclose(best_mse, 0, atol=TOLERANCE):
                     match_locations.append((y, x1, best_mse_x2))
 
     # print position map
     np.set_printoptions(threshold=np.inf)
+    print("Match locations: ", match_locations)
     match_locations.sort()
     # print(match_locations)
 
@@ -110,31 +152,57 @@ def find_best_match(image1, image2, kernel_size):
 
     return match_map
 
+def superimpose(img1, img2, alpha):
+    # alpha: transparency value for blending for alpha 1
+
+    # shape[1]: width, shape[0]: height
+    img2_resized = cv2.resize(img2, (img1.shape[1], img1.shape[0]))
+    blended = cv2.addWeighted(img1, alpha, img2_resized, 1-alpha, 0)
+
+    return blended
+
 
 def main():
+
     img1 = initilizeImage(IMG1_PATH, IMG_CROP_START_X,
                           IMG_CROP_START_Y, IMG_CROP_WIDTH, IMG_CROP_HEIGHT)
     img2 = initilizeImage(IMG2_PATH, IMG_CROP_START_X,
                           IMG_CROP_START_Y, IMG_CROP_WIDTH, IMG_CROP_HEIGHT)
-
-    # Images before
+    
+    # Initial Images
     imagesBefore = np.concatenate((img1, img2), axis=1)
-    cv2.namedWindow('Resized Image', cv2.WINDOW_NORMAL)
     cv2.imshow("Initial images", imagesBefore)
+    # cv2.waitKey(0)
+
+    r1, r2 = extractColors(img1, img2, 'red')
+    g1, g2 = extractColors(img1, img2, 'green')
+    b1, b2 = extractColors(img1, img2, 'blue')
+
+    # cv2.namedWindow(f"Extracted red", cv2.WINDOW_NORMAL)
+    # cv2.imshow(f"Extracted red", np.concatenate((r1, r2), axis=1))
+
+
+    # Standard
+    edge1 = detectEdges(img1)
+    edge2 = detectEdges(img2)
+    # cv2.imshow('Canny Edge Detection', np.concatenate((edge1, edge2), axis=1))
+
+    # Color
+    edge1_r = detectEdges(r1)
+    edge2_r = detectEdges(r2)
+    # cv2.imshow('Canny Edge Detection w color', np.concatenate((edge1_r, edge2_r), axis=1))
     cv2.waitKey(0)
 
-    # Brain process
-    visualCortexV1()
-    img1Edges = visualCortexV2V3(img1)
-    img2Edges = visualCortexV2V3(img2)
-    imagesEdges = np.concatenate((img1Edges, img2Edges), axis=1)
-    cv2.namedWindow('Resized Image', cv2.WINDOW_NORMAL)
-    cv2.imshow('Canny Edge Detection', imagesEdges)
-    cv2.waitKey(0)
+    blended_initial = superimpose(edge1, edge2, 0.3)
+    cv2.imshow("Edges Superimposed", blended_initial)
 
-    match_map = visualCortexV4(img1Edges, img2Edges)
+    blended_r = superimpose(edge1_r, edge2_r, 0.3)
+    cv2.imshow("Red Edges Superimposed", blended_r)
+
+
+    match_map = convolute(edge1, edge2, KERNEL_SIZE)
     cv2.imshow('Match Map', match_map)
-    # print(match_map)
+    # # print(match_map)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
